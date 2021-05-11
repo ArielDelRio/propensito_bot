89ccad3759189d7d8940febf7ebad24953549a7a
ArieldRio <arieldelrioviamonte@gmail.com>
2021-05-11 14:39:04 -0500

    Set quote to None in reply_text messages

--- propensito_bot.py
+++ propensito_bot.py
@@ -24,22 +24,6 @@ def get_players_ready_message(chat_id):
             message += '\n' + user.first_name
     return message
 
-def get_inline_menu():
-    return [
-        InlineQueryResultArticle(
-            id=str(uuid4()),
-            title="Main Menu",
-            input_message_content=InputTextMessageContent(
-                get_players_ready_message()),
-            reply_markup=InlineKeyboardMarkup(MAIN_MENU_KEYBOARD)
-        ),
-        InlineQueryResultArticle(
-            id=str(uuid4()),
-            title="Help message",
-            input_message_content=InputTextMessageContent("Help message"),
-        ),
-    ]
-
 
 def add_player(chat_id, user):
     if chat_id in game_rooms.keys():
@@ -53,39 +37,41 @@ def add_player(chat_id, user):
 
 
 MAIN_MENU_KEYBOARD = [
-        [
-            InlineKeyboardButton("Unirse", callback_data='join'),
-            InlineKeyboardButton("Empezar", callback_data='start'),
-        ],
-        [InlineKeyboardButton("X", callback_data='3')],
-    ]
+    [
+        InlineKeyboardButton("Unirse", callback_data='join'),
+        InlineKeyboardButton("Empezar", callback_data='start'),
+    ],
+    [InlineKeyboardButton("X", callback_data='3')],
+]
 
 
 def start(update: Update, _: CallbackContext) -> None:
-    update.message.reply_text('Hello World')
+    update.message.reply_text('Hello World', quote=False)
+
 
 def help(update: Update, _: CallbackContext) -> None:
-    update.message.reply_text('Help Message')
+    update.message.reply_text('Help Message', quote=False)
+
 
 def main_menu(update: Update, _: CallbackContext) -> None:
     chat_id = update.message.chat.id
-    update.message.reply_text(text=get_players_ready_message(chat_id), reply_markup=InlineKeyboardMarkup(MAIN_MENU_KEYBOARD))
+    update.message.reply_text(text=get_players_ready_message(
+        chat_id), reply_markup=InlineKeyboardMarkup(MAIN_MENU_KEYBOARD), quote=False)
+
 
 def callback_handler(update: Update, _: CallbackContext) -> None:
     query = update.callback_query
 
     query.answer()
+
     chat_id = query.message.chat.id
-    if chat_id in chats.keys():
-        if query.from_user not in chats[chat_id]:
-            chats[chat_id].append(query.from_user)
-    else:
-        chats[chat_id] = [query.from_user]
+    user = query.from_user
 
-    query.edit_message_text(text=get_players_ready_message(chat_id), reply_markup=InlineKeyboardMarkup(MAIN_MENU_KEYBOARD))
+    added = add_player(chat_id, user)
 
-def inlinequery(update: Update, _: CallbackContext) -> None:
-    update.inline_query.answer(get_inline_menu())
+    if added:
+        query.edit_message_text(text=get_players_ready_message(
+            chat_id), reply_markup=InlineKeyboardMarkup(MAIN_MENU_KEYBOARD))
 
 
 def main():
@@ -97,9 +83,6 @@ def main():
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help))
     dispatcher.add_handler(CommandHandler("main_menu", main_menu))
-
-    # For @bot_name inline command use
-    dispatcher.add_handler(InlineQueryHandler(inlinequery))
 
     dispatcher.add_handler(
         CallbackQueryHandler(callback_handler))
