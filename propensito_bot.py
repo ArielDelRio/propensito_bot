640ee6c5844c31fd32235e2829ebe02cc020e2dd
ArieldRio <arieldelrioviamonte@gmail.com>
2021-05-12 15:57:45 -0500

    Add ConversationHandler for best flow control
    Also add Start and Exit functionalities

--- propensito_bot.py
+++ propensito_bot.py
@@ -1,6 +1,6 @@
 from uuid import uuid4
 from telegram import InlineQueryResultArticle, InputTextMessageContent, Update, InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler, CallbackContext, CallbackQueryHandler
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler, CallbackContext, CallbackQueryHandler, ConversationHandler
 import logging
 import os
 
@@ -11,9 +11,28 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 game_rooms = {}
 
+# Stages
+PREPARE_GAME, IN_GAME = range(2)
+
+# Callback data
+JOIN, START, EXIT = range(3)
+
 
 def get_players_ready_message(chat_id):
     message = "Esperando jugadores...\nUnidos: "
+
+    if not chat_id:
+        return message
+
+    if chat_id in game_rooms:
+        users = game_rooms[chat_id]
+        for user in users:
+            message += '\n' + user.first_name
+    return message
+
+
+def get_players_in_game_message(chat_id):
+    message = "Jugadores: \n"
 
     if not chat_id:
         return message
@@ -36,30 +55,50 @@ def add_player(chat_id, user):
         return True
 
 
+def clear_room(chat_id):
+    if chat_id in game_rooms:
+        game_rooms.pop(chat_id)
+
+
 MAIN_MENU_KEYBOARD = [
     [
-        InlineKeyboardButton("Unirse", callback_data='join'),
-        InlineKeyboardButton("Empezar", callback_data='start'),
+        InlineKeyboardButton("Unirse", callback_data=str(JOIN)),
+        InlineKeyboardButton("Empezar", callback_data=str(START)),
     ],
-    [InlineKeyboardButton("X", callback_data='3')],
+    [InlineKeyboardButton("X", callback_data=str(EXIT))],
+]
+
+MAIN_MENU_IN_GAME = [
+    [InlineKeyboardButton("X", callback_data=str(EXIT))],
 ]
 
 
 def start(update: Update, _: CallbackContext) -> None:
-    update.message.reply_text('Hello World', quote=False)
+    update.message.reply_text(text='Bienvenido a Propensito Game\nEsperando jugadores...\n', reply_markup=InlineKeyboardMarkup(
+        MAIN_MENU_KEYBOARD), quote=False)
+    return PREPARE_GAME
 
 
 def help(update: Update, _: CallbackContext) -> None:
     update.message.reply_text('Help Message', quote=False)
+    return PREPARE_GAME
 
 
 def main_menu(update: Update, _: CallbackContext) -> None:
     chat_id = update.message.chat.id
     update.message.reply_text(text=get_players_ready_message(
         chat_id), reply_markup=InlineKeyboardMarkup(MAIN_MENU_KEYBOARD), quote=False)
+    return PREPARE_GAME
 
 
-def callback_handler(update: Update, _: CallbackContext) -> None:
+def main_menu_in_game(update: Update, _: CallbackContext) -> None:
+    chat_id = update.message.chat.id
+    update.message.reply_text(text=get_players_in_game_message(
+        chat_id), reply_markup=InlineKeyboardMarkup(MAIN_MENU_IN_GAME), quote=False)
+    return IN_GAME
+
+
+def join(update: Update, _: CallbackContext) -> None:
     query = update.callback_query
 
     query.answer()
@@ -73,6 +112,23 @@ def callback_handler(update: Update, _: CallbackContext) -> None:
         query.edit_message_text(text=get_players_ready_message(
             chat_id), reply_markup=InlineKeyboardMarkup(MAIN_MENU_KEYBOARD))
 
+    return PREPARE_GAME
+
+
+def start_game(update: Update, _: CallbackContext):
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text="Start the Game")
+    return IN_GAME
+
+
+def exit(update: Update, _: CallbackContext):
+    query = update.callback_query
+    clear_room(query.message.chat_id)
+    query.answer()
+    query.edit_message_text(text="See you next time!")
+    return ConversationHandler.END
+
 
 def main():
     updater = Updater(
@@ -80,12 +136,29 @@ def main():
 
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help))
-    dispatcher.add_handler(CommandHandler("main_menu", main_menu))
 
-    dispatcher.add_handler(
-        CallbackQueryHandler(callback_handler))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            PREPARE_GAME: [
+                CommandHandler("main_menu", main_menu),
+                CallbackQueryHandler(join, pattern='^' + str(JOIN) + '$'),
+                CallbackQueryHandler(
+                    start_game, pattern='^' + str(START) + '$'),
+            ],
+            IN_GAME: [
+                CommandHandler("main_menu", main_menu_in_game),
+
+            ],
+        },
+        fallbacks=[CallbackQueryHandler(exit, pattern='^' + str(EXIT) + '$')],
+    )
+
+    dispatcher.add_handler(conv_handler)
+
+    # dispatcher.add_handler(
+    #     CallbackQueryHandler(main_menu_callback_handler))
 
     updater.start_polling()
     updater.idle()
