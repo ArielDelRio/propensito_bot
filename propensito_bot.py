c862839df3d7a6b9b94d98f71efb2ec8aeb61879
ArieldRio <arieldelrioviamonte@gmail.com>
2021-05-14 14:12:10 -0500

    Add send_poll and recive_poll_answer for questions game

--- propensito_bot.py
+++ propensito_bot.py
@@ -1,8 +1,12 @@
 from uuid import uuid4
-from telegram import InlineQueryResultArticle, InputTextMessageContent, Update, InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler, CallbackContext, CallbackQueryHandler, ConversationHandler
+from telegram import InlineQueryResultArticle, InputTextMessageContent, Update, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardRemove, KeyboardButton, KeyboardButtonPollType, ReplyKeyboardMarkup, ParseMode
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler, CallbackContext, CallbackQueryHandler, PollHandler, ConversationHandler, MessageHandler, PicklePersistence
 import logging
 import os
+
+from telegram.ext.basepersistence import BasePersistence
+from telegram.ext.filters import Filters
+from telegram.ext.pollanswerhandler import PollAnswerHandler
 
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -58,6 +62,33 @@ def add_player(chat_id, user):
 def clear_room(chat_id):
     if chat_id in game_rooms:
         game_rooms.pop(chat_id)
+
+
+def send_poll(chat_id, context):
+    players = chat_id in game_rooms and game_rooms[chat_id]
+
+    if not players:
+        raise ValueError
+
+    message = context.bot.send_poll(
+        chat_id,
+        "Primera Pregunta",
+        players,
+        is_anonymous=False,
+        allows_multiple_answers=True,
+    )
+
+    # Save some info about the poll the bot_data for later use in receive_poll_answer
+    payload = {
+        message.poll.id: {
+            "questions": players,
+            "message_id": message.message_id,
+            "chat_id": chat_id,
+            "answers": 0,
+        }
+    }
+
+    context.bot_data.update(payload)
 
 
 MAIN_MENU_KEYBOARD = [
@@ -119,6 +150,9 @@ def start_game(update: Update, _: CallbackContext):
     query = update.callback_query
     query.answer()
     query.edit_message_text(text="Start the Game")
+
+    send_poll(update.effective_chat.id, _)
+
     return IN_GAME
 
 
@@ -130,9 +164,29 @@ def exit(update: Update, _: CallbackContext):
     return ConversationHandler.END
 
 
+def receive_poll_answer(update: Update, _: CallbackContext):
+    answer = update.poll_answer
+    poll_id = answer.poll_id
+    try:
+        questions = _.bot_data[poll_id]["questions"]
+    # this means this poll answer update is from an old poll, we can't do our answering then
+    except KeyError:
+        return
+    selected_options = answer.option_ids
+    answer_string = ""
+    for question_id in selected_options:
+        answer_string += questions[question_id]
+    _.bot.send_message(
+        _.bot_data[poll_id]["chat_id"],
+        f"{update.effective_user.first_name} feels {answer_string}!",
+    )
+
+
 def main():
+    persistence = PicklePersistence(filename='conversationbot')
+
     updater = Updater(
-        token=os.environ.get('TOKEN'), use_context=True)
+        token=os.environ.get('TOKEN'), persistence=persistence, use_context=True)
 
     dispatcher = updater.dispatcher
 
@@ -149,11 +203,16 @@ def main():
             ],
             IN_GAME: [
                 CommandHandler("main_menu", main_menu_in_game),
-
             ],
         },
         fallbacks=[CallbackQueryHandler(exit, pattern='^' + str(EXIT) + '$')],
+        per_user=False,
+        per_message=False,
+        name="GameHandler",
+        # persistent=True
     )
+
+    dispatcher.add_handler(PollAnswerHandler(receive_poll_answer))
 
     dispatcher.add_handler(conv_handler)
 
